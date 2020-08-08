41e6cfd39e11977ba270b35351514ef96953b94a
SudeshGagare <SudeshGagare@users.noreply.github.com>
2020-08-08 15:47:30 +0530

    Update Polygon.cpp

--- Polygon.cpp
+++ Polygon.cpp
@@ -1,14 +1,14 @@
 #include <iostream> 
 using namespace std; 
-#define INF 10000 
+#define MAXX 10000 
 
-struct Point 
+struct Coordinate
 { 
 	double x; 
 	double y; 
 }; 
 
-bool onSegment(Point p, Point q, Point r) 
+bool PresentOnLine(Coordinate p, Coordinate q, Coordinate r) 
 { 
 	if (q.x <= max(p.x, r.x) && q.x >= min(p.x, r.x) && 
 			q.y <= max(p.y, r.y) && q.y >= min(p.y, r.y)) 
@@ -16,7 +16,7 @@ bool onSegment(Point p, Point q, Point r)
 	return false; 
 } 
 
-int orientation(Point p, Point q, Point r) 
+int Direction(Coordinate p, Coordinate q, Coordinate r) 
 { 
 	int val = (q.y - p.y) * (r.x - q.x) - 
 			(q.x - p.x) * (r.y - q.y); 
@@ -24,35 +24,35 @@ int orientation(Point p, Point q, Point r)
 	return (val > 0)? 1: 2; 
 } 
 
-bool doIntersect(Point p1, Point q1, Point p2, Point q2) 
+bool WhetherIntersect(Coordinate p1, Coordinate q1, Coordinate p2, Coordinate q2) 
 { 
-	int o1 = orientation(p1, q1, p2); 
-	int o2 = orientation(p1, q1, q2); 
-	int o3 = orientation(p2, q2, p1); 
-	int o4 = orientation(p2, q2, q1); 
+	int o1 = direction(p1, q1, p2); 
+	int o2 = direction(p1, q1, q2); 
+	int o3 = direction(p2, q2, p1); 
+	int o4 = direction(p2, q2, q1); 
 	
 	if (o1 != o2 && o3 != o4) 
 		return true; 
 
-	if (o1 == 0 && onSegment(p1, p2, q1)) return true; 
-	if (o2 == 0 && onSegment(p1, q2, q1)) return true; 
-	if (o3 == 0 && onSegment(p2, p1, q2)) return true; 
-	if (o4 == 0 && onSegment(p2, q1, q2)) return true; 
+	if (o1 == 0 && PresentOnLine(p1, p2, q1)) return true; 
+	if (o2 == 0 && PresentOnLine(p1, q2, q1)) return true; 
+	if (o3 == 0 && PresentOnLine(p2, p1, q2)) return true; 
+	if (o4 == 0 && PresentOnLine(p2, q1, q2)) return true; 
 	return false; 
 } 
 
-bool isInside(Point polygon[], Point p) 
+bool WhetherPointIsInside(Coordinate polygon[], Coordinate p) 
 {   int n = (sizeof(*polygon));
 	if (n < 3) return false; 
-	Point extreme = {INF, p.y}; 
+	Coordinate extreme = {MAXX, p.y}; 
 	int count = 0, i = 0; 
 	do
 	{ 
 		int next = (i+1)%n; 
-		if (doIntersect(polygon[i], polygon[next], p, extreme)) 
+		if (WhetherIntersect(polygon[i], polygon[next], p, extreme)) 
 		{ 
-			if (orientation(polygon[i], p, polygon[next]) == 0) 
-			return onSegment(polygon[i], p, polygon[next]); 
+			if (direction(polygon[i], p, polygon[next]) == 0) 
+			return PresentOnLine(polygon[i], p, polygon[next]); 
 			count++; 
 		} 
 		i = next; 
@@ -63,13 +63,13 @@ bool isInside(Point polygon[], Point p)
 
 int main() 
 { 
-	Point polygon1[] = {{1, 0}, {8, 3}, {8, 8}, {1, 5}}; 
-	Point p = {3, 5}; 
-	isInside(polygon1, p)? cout << "True \n": cout << "False \n"; 
+	Coordinate polygon1[] = {{1, 0}, {8, 3}, {8, 8}, {1, 5}}; 
+	Coordinate p = {3, 5}; 
+	WhetherPointIsInside(polygon1, p)? cout << "True \n": cout << "False \n"; 
 
-	Point polygon2[] = {{-3, 2}, {-2, -0.8}, {0, 1.2},{2.2,0},{2,4.5}}; 
+	Coordinate polygon2[] = {{-3, 2}, {-2, -0.8}, {0, 1.2},{2.2,0},{2,4.5}}; 
 	p = {0, 0}; 
-	isInside(polygon2, p)? cout << "True \n": cout << "False \n"; 
+	WhetherPointIsInside(polygon2, p)? cout << "True \n": cout << "False \n"; 
 	
 	return 0; 
 }
